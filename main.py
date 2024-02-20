23b0690f8be7f14100028cc0fc11b4ae3ed7250e
Pick-Y <a.scarcella.as@gmail.com>
2024-02-21 00:36:41 +1100

    Added for loop to loop through filepaths

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ from pathlib import Path
 #Create a filepath for all files ending in xlsx
 
 filepaths = glob.glob("invoices/*.xlsx")
+
+#Create loop to loop through paths stored in filepaths
+for filepath in filepaths:
+    
+   
+    pdf = FPDF("P")
+    pdf.add_page()
+
+pdf.output(f'pdfs/invoice-{invoice_nr}.pdf', 'F')
