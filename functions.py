3a4d219de7c3962d92536ae8df604a9611d66dd6
Pick-Y <a.scarcella.as@gmail.com>
2024-03-05 08:38:04 +1100

    Added method to print amount due

--- functions.py
+++ functions.py
@@ -50,6 +50,24 @@ class Pdf_invoice ():
         self.pdf.cell(30,8,column_replaced[3],border=1)
         self.pdf.cell(30,8,column_replaced[4],border=1,ln=1)
     
+    def fillTable(self):
+        for index,row in self.df.iterrows():
+       
+            self.pdf.set_font(family="Times", size=10)
+            self.pdf.set_text_color(80,80,80)
+        
+            self.pdf.cell(20,8,str(row['product_id']),border=1)
+            self.pdf.cell(50,8,str(row['product_name']), border=1)
+            self.pdf.cell(30,8,str(row['amount_purchased']),border=1)
+            self.pdf.cell(30,8,str(row['price_per_unit']),border=1)
+            self.pdf.cell(30,8,str(row['total_price']),border=1, ln=1)
+
+    def printAmountDue(self):
+        total = self.df['total_price'].sum()
+        self.pdf.set_font(family="Times", size=10)
+        self.pdf.set_text_color(0,0,0)
+        self.pdf.cell(30,40,f"The total amount due is {total}") 
+
     def pdf_output(self):
         newString = self.invoice_number + "-" + self.date
         self.pdf.output(f'pdfs/{newString}.pdf', 'F')
